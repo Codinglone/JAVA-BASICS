93fd09c1f54d75375934626cf2d96e2860d8fdb9
Fabrice <codinglone@gmail.com>
2023-04-27 17:26:17 +0200

    Trying...

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("I am codinglone!");
+        string names = "Fabrice";
+        int num = 100;
+
+        if(num > 1){
+            System.out.println("Num is greater than 1");
+        }
+
     }
 }
